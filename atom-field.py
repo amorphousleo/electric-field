95d58badc2fdbcfd38fc603baac8f00a9a7e4ecc
amorphousleo <amorphousleo@users.noreply.github.com>
2018-11-30 17:08:10 -0500

    update columb calc, new csv
    From jupyter notebook output csv file for collaboration. Update the columb code to have distance properly squared. Added a save for the coordinates numpy file.

--- atom-field.py
+++ atom-field.py
@@ -3,7 +3,7 @@
 Created on Tue Nov 27 10:58:10 2018
 
 @author: Leo-Desktop
-"""
+""" 
 
 import pymatgen as pmg
 import numpy as np
@@ -36,6 +36,7 @@ def kdtree(structure, inner_cutoff_radius=1.0, outer_cutoff_radius=3.0):
     offset = np.array([structure.lattice.a, structure.lattice.b, structure.lattice.c])
     supercell_coordinates = supercell_structure.cart_coords - offset #np array of all coordinates
     coordinates = structure.cart_coords
+    np.save('data/results/coordinates.npy', structure.cart_coords) # save coordinates as np array
     charges = np.array([getattr(site.specie, "oxi_state", 0) for site in supercell_structure])
     
     # Now build the KTTree
@@ -55,7 +56,7 @@ def calculate_electric_field(coordinates, supercell_coordinates,inner_cutoff_rad
         for j in atom_indicies[n]:
             dist = np.linalg.norm(coordinates[n]-supercell_coordinates[j])
             if dist > 1.0:
-                grid_efield[n] += ((coordinates[n]-supercell_coordinates[j])*((COLUMB * E * charges[n]) / (dist)))
+                grid_efield[n] += ((coordinates[n]-supercell_coordinates[j])*((COLUMB * E * charges[n]) / (dist**2)))
         n += 1
     return grid_efield
         
